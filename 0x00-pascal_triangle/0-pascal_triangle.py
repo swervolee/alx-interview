6628197e742790e63abc601d5b4fce4a72fc3bb4
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-01 20:09:31 +0300

    .

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    """
+    ALX interview pascal triangle
+    """
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) != n:
+        row = triangle[-1]
+        new_row = [1]
+        for i in range(len(row) - 1):
+            new_row.append(row[i] + row[i + 1])
+        new_row.append(1)
+        triangle.append(new_row)
+    return triangle
