079a9afd29b078c2b70031fc99cab0675e4e5447
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-07 11:14:53 +0300

    alx interview question

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
+"""
+ALX INTERVIEW QUESTION
+"""
+
 
 def canUnlockAll(boxes):
     """
     ALX INTERVIEW QUESTION
     """
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    keys = boxes[0]
+
+    while keys:
+        new_key = keys.pop()
+        if new_key < n and not opened[new_key]:
+            opened[new_key] = True
+            keys.extend(boxes[new_key])
+    return all(opened)
