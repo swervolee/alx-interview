ce9bc16248296d23b080a0dbfa9ba1786f747d11
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-20 13:16:04 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,80 +3,35 @@
 LOG PARSING
 """
 import sys
-from datetime import datetime
-from typing import List, Union
-import select
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def check_format(line: str) -> Union[List[int], bool]:
-    """
-    Checks if the line has the format
-    <IP Address> - [<date>] "GET /projects/260
-    HTTP/1.1" <status code> <file size>
-    """
-    status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-    try:
-        args = line.split(" - ")
-        ip = args[0]
-
-        if ip.count(".") != 3:
-            return False
-
-        format_string = "%Y-%m-%d %H:%M:%S.%f"
-
-        args = args[1].split("] ")
-        date_string = args[0][1:]
-
-        try:
-            real_date = datetime.strptime(date_string, format_string)
-        except Exception:
-            return False
-
-        args = args[1].split('" ')
-
-        request = args[0].strip('"')
-        if request != "GET /projects/260 HTTP/1.1":
-            return False
-
-        code, file_size = int(args[1].split()[0]), int(args[1].split()[1])
-
-        if code not in status_codes:
-            return False
-
-        return [code, file_size]
-    except Exception:
-        return False
-
-
-def print_info(file_size, codes):
-    """
-    prints info
-    """
-    response_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-    print(f"File size: {file_size}")
-    for code in sorted(set(codes)):
-        if code in response_codes:
-            print(f"{code}: {codes.count(code)}")
-
-
-if __name__ == "__main__":
-    file_size = 0
-    codes = []
-    counter = 0
-
-    while True:
-        try:
-            for line in sys.stdin:
-                if counter == 10:
-                    print_info(file_size, codes)
-                    counter = 0
-                result = check_format(line)
-                if result:
-                    codes.append(result[0])
-                    file_size += result[1]
-                counter += 1
-        except KeyboardInterrupt:
-            print_info(file_size, codes)
+        if counter == 10:
             counter = 0
-            continue
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
