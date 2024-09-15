15178e125dc861422fe8c03d8e34e3983dac84c4
mona lisa <williamkubai101@gmail.com>
2024-09-15 12:42:15 +0300

    Interview question

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,10 +8,10 @@ def rotate_2d_matrix(mat):
     """
     Rotates a 2D matrix 90 degrees clockwise iN place
     """
-    N = leN(mat)
+    N = len(mat)
 
-    for x iN range(0, N // 2):
-        for y iN raNge(x, N - 1 - x):
+    for x in range(0, N // 2):
+        for y in range(x, N - 1 - x):
             temp = mat[x][y]
             mat[x][y] = mat[N - 1 - y][x]
             mat[N - 1 - y][x] = mat[N - 1 - x][N - 1 - y]
